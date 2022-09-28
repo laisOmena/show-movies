b26815bd40d586fde00cb97421f9fe0d5a934668
Lais Omena <laisf.omena@gmail.com>
2022-09-28 11:44:37 +0100

    Criando search

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,35 @@
+import { useState } from 'react';
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi';
 
 import './NavBar.css';
 
 const NavBar = () => {
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    if(!search) return;
+
+    navigate(`/search?q=${search}`);
+    setSearch('');
+  };
+
   return (
     <nav id="navbar">
         <h2>
           <Link to="/"><BiCameraMovie /> showMovies</Link>
         </h2>
-        <form>
-          <input type="text" placeholder="Search for a movie" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Search for a movie"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          />
           <button type="submit">
               <BiSearchAlt2 />
           </button>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,45 @@
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import MovieCard from '../components/MovieCard';
+
+import './MovieGrid.css';
+
+const searchUrl = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+
 const Search = () => {
+  const [searchParams] = useSearchParams();
+
+  const [movies, setMovies] = useState([]);
+  const query = searchParams.get('q');
+
+  const getSearchedMovies = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    setMovies(data.results);
+  }
+  
+  useEffect(() => {
+    const searchWithQueryUrl = `${searchUrl}?${apiKey}&query=${query}`;
+
+    getSearchedMovies(searchWithQueryUrl);
+  }, [query]);
+
+
   return ( 
-    <div>Search</div>
+    <div className='container'>
+      <h2 className="title">
+        Results for: <span className="query-text">{query}</span>
+      </h2>
+      <div className="movies-container">
+        {movies.length > 0
+           && movies
+            .map((movie) => <MovieCard key={movie.id} movie={movie} />)
+        }
+      </div>
+    </div>
    );
 }
  
